d9add2880908041aa945e1d88c00620e2170a3e2
kevinjacob10 <61705265+kevinjacob10@users.noreply.github.com>
2021-09-21 05:17:29 -0400

    Sprites for Ping and Mouse3d position code

---
+++ Assets/Scripts/PingSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PingSystem 
+{
+    public static void AddPing(Vector3 position)
+    {
+        Object.Instantiate(GameAssets.i.PingWorld, position, Quaternion.identity);
+    }
+}
